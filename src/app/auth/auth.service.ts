75de96f3d876efacb6a06f09fe2f76d399cd3f79
EihabAhmed <eihabahmed@gmail.com>
2023-01-12 07:50:18 +0200

    Implemented register page

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { RegisterPayload } from './register-payload';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'http://localhost:8080/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  register(registerPayload: RegisterPayload): Observable<any> {
+    return this.httpClient.post(this.url + 'signup', registerPayload)
+  }
+}
