4b4984e1ff5ffcf04fd4d63efa9a2c6418bcc4d0
EihabAhmed <eihabahmed@gmail.com>
2023-01-13 10:49:16 +0200

    Fixed CORS problem (part 9)

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { RegisterPayload } from './register-payload';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,11 +8,11 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  private url = 'http://localhost:8080/';
+  private url = 'http://localhost:8080/api/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
   register(registerPayload: RegisterPayload): Observable<any> {
-    return this.httpClient.post(this.url + 'signup', registerPayload)
+    return this.httpClient.post(this.url + 'signup', registerPayload);
   }
 }
