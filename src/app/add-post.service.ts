2e5c877449c09767db59c6fa3912f957361da768
EihabAhmed <eihabahmed@gmail.com>
2023-01-14 20:17:26 +0200

    Display Posts (part 13)

--- src/app/add-post.service.ts
+++ src/app/add-post.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { PostPayload } from './add-post/post-payload';
 
 @Injectable({
@@ -11,5 +12,13 @@ export class AddPostService {
 
   addPost(postPayload: PostPayload) {
     return this.httpClient.post('http://localhost:8080/api/posts', postPayload);
+  }
+
+  getAllPosts(): Observable<Array<PostPayload>> {
+    return this.httpClient.get<Array<PostPayload>>("http://localhost:8080/api/posts/all");
+  }
+
+  getPost(permaLink: Number): Observable<PostPayload> {
+    return this.httpClient.get<PostPayload>('http://localhost:8080/api/posts/get/' + permaLink);
   }
 }
